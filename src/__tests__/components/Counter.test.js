9628f2f3bc702cab4bfcf84ad0e99a4e1ab30616
Matt Wilkin <mwilkin22@gmail.com>
2019-07-05 13:52:33 -0700

    refactor test for counter, working.

--- src/__tests__/components/Counter.test.js
+++ src/__tests__/components/Counter.test.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
-// import React from 'react';
+import React from 'react';
 // import renderer from 'react-test-renderer';
-// import Counter from '../../components/Counter/Counter';
+import Counter from '../../components/Counter/Counter';
 
 describe('<Counter /> Enzyme Test', () => {
-  it('should make Travis pass', () => {
-    expect(1).toEqual(1);
+  it('is alive at application start', () => {
+    // eslint-disable-next-line no-undef
+    const app = shallow(<Counter/>);
+    expect(app.find('.count').text()).toBe('0');
   });
 });
